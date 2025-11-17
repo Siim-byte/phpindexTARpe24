26db6bf4ec205fcefbaae338dd3556c20f8c7a1b
Siim Beloi <beloimiis@gmail.com>
2025-11-17 12:13:17 +0200

    Git käsud lisatud2

--- content/gitKasud.php
+++ content/gitKasud.php
@@ -14,17 +14,23 @@ git config --global --list
 </li>
 <li>
 //ssh võti loomine
+    <pre>
 ssh-keygen -o -t rsa -C "beloimiis@gmail.com"
 <br>//võti salvestatakse opilane/.ssh kausta
 <br>//id_rsa.pub tuleb kopeerida oma git hub -reposse
- </li>
+</pre>
+</li>
  <li>
+     <pre>
   git add .
  git commit -a -m "on loodud phpIndex"
  git remote remove origin
+         </pre>
 </li>
 <li>
+    <pre>
 git remote add origin git@github.com:Siim-byte/phpindexTARpe24.git
 git branch -M main
 git push -u origin main
+        </pre>
 </li>

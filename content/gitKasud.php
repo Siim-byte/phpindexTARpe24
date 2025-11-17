d5ad72f04cb1083047425e21e7bc8761567955b2
Siim Beloi <beloimiis@gmail.com>
2025-11-17 12:33:56 +0200

    fixed

--- content/gitKasud.php
+++ content/gitKasud.php
@@ -3,28 +3,43 @@ echo "<h2>GIT CMD käsurida</h2>";
 echo "<ul>";
 echo "<li>git init - uus repo</li>";
 ?>
-<li>
-    <pre>
-git config --global user.name "Siim Beloi"
 
+<pre>
+<li>
+git init - uus repo
+
+git config --global user.name "Siim Beloi""
+</li>
+<li>
 git config --global user.email "beloimiis@gmail.com"<br>
 
 git config --global --list
-    </pre>
+</li>
+    </li>;
+<li>
+ //ssh võti loomine
+ <pre>
+ ssh-keygen -o -t rsa -C "beloimiis@gmail.com"
+</pre>
+
+<br>// võti salvestataks opilane/.shh kasuta
+<br>//id_rsa.pub tuleb kopeerida oma Github -reposse
 </li>
 <li>
-//ssh võti loomine
-ssh-keygen -o -t rsa -C "beloimiis@gmail.com"
-<br>//võti salvestatakse opilane/.ssh kausta
-<br>//id_rsa.pub tuleb kopeerida oma git hub -reposse
- </li>
- <li>
-  git add .
- git commit -a -m "on loodud phpIndex"
- git remote remove origin
+git add.
+
+git commint -a -m "onloodud phpIndex"
+
+git remote remove origin
 </li>
 <li>
+//
+<pre>
 git remote add origin git@github.com:Siim-byte/phpindexTARpe24.git
+</pre>
+
 git branch -M main
+
 git push -u origin main
 </li>
+</pre>

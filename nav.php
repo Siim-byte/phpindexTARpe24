8d92ce194d15187bc9733fbce31cb27de46ed1b7
Siim-byte <beloimiis@gmail.com>
2025-12-04 19:59:14 +0200

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,51 @@
+
+<nav class="menu">
+    <ul>
+        <li>
+            <a href="?leht=kodu.php">Kodu</a>
+        </li>
+        <li>
+            <a href="?leht=JsToo.php">JS joonis</a>
+        </li>
+        <li>
+            <a href="?leht=muusika.php">JS Muusika küsitlus</a>
+        </li>
+        <li>
+            <a href="#">Funktsioonid</a>
+            <ul class="dropdown">
+                <li>
+                    <a href="?leht=ajafunktsioonid.php">Ajafunktsioonid</a>
+                </li>
+                <li>
+                    <a href="?leht=tekstifunktsioonid.php">Tekstifunktsioonid</a>
+                </li>
+                <li>
+                    <a href="?leht=matemaatilised.php">Matemaatilised</a>
+                </li>
+            </ul>
+        </li>
+            <li>
+                <a href="https://siimbeloi24.thkit.ee" target="_blank">Vana index.html</a>
+            </li>
+          <li>
+                <a href="?leht=gitKasud.php">GIT Käsud</a>
+            </li>
+        <li>
+            <a href="?leht=galerii.php">Galerii</a>
+        </li>
+                <li>
+            <a href="#">Mobiilimall</a>
+        <ul class="dropdown">
+                <li>
+                    <a href="/phpindex/content/mobiilimall/blankett.html">Tunniplaan</a>
+                </li>
+            <li>
+                    <a href="/phpindex/content/mobiilimall/blankett2.html">Anekdoodid</a>
+                </li>
+        </ul>
+        </li>
+        <li>
+        <a href="content/andmebaasloomad/loomadeKuvamine.php" target="_blank">Loomade andmebaas</a>
+        </li>
+        </ul>
+</nav>
